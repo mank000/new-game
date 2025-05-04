7e643d9c9ef6d149530af9687f1fb46152a2cc3d
mank000 <kozmin_arti@mail.ru>
2025-05-04 22:06:18 +0700

    skibidi2

--- scripts/wolf.gd
+++ scripts/wolf.gd
@@ -2,28 +2,28 @@ extends CharacterBody2D
 
 const SPEED := 100.0
 const JUMP_VELOCITY := -200.0
-const GRAVITY := 900.0  # гравитация, подбирай по размеру игры
+const GRAVITY := 900.0
 
 @onready var target := get_node("/root/Game/PLAYER")
+@onready var obstacle_ray := $RayCast2D
 
 func _physics_process(delta: float) -> void:
-	# Применяем гравитацию
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 	else:
-		velocity.y = 0  # сбрасываем вертикальную скорость, когда на земле
+		velocity.y = 0
 
 	if target:
 		var direction = target.global_position - global_position
 
-		# Горизонтальное движение
-		if abs(direction.x) > 10:  # чтобы не дёргался на месте
+		if abs(direction.x) > 10:
 			velocity.x = sign(direction.x) * SPEED
+			obstacle_ray.scale.x = sign(direction.x)  # зеркалируем луч
 		else:
 			velocity.x = 0
 
-		# Прыжок, если цель выше на 20 пикселей
-		if direction.y < -15 and is_on_floor():
+		# Прыгаем, если впереди стена
+		if obstacle_ray.is_colliding() and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 
 	move_and_slide()
